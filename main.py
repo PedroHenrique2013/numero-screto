c2a2bf3947b86637ed60adad5eb21d212127bb19
PedroHenrique2013 <pedrocapa2505@gmail.com>
2026-08-06 19:36:15 -0300

    alterando limite de idade

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-idade = input("informe sua idade: ")
+# 1. Pede a idade ao usuário e converte para número inteiro
+idade = int(input("Informe sua idade: "))
 
-if idade >= 16:
-    print("Permutido!")
+# 2. Verifica a condição
+if idade < 18:
+    print("Bloqueado!")
 else:
-    print("bloqueado!")
+    print("Permitido!")
